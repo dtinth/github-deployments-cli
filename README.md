6913e3ca79f7966e583623cd7b9d01a80903e0ca
Thai Pangsakulyanont <dtinth@spacet.me>
2020-03-30 14:33:00 +0700

    Turn it into github-deployments-cli

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-# obtain-github-app-installation-access-token
+# github-deployments-cli
 
-A simple CLI to obtain a GitHub App Installation Access Token.
-
-```
-npx obtain-github-app-installation-access-token \
-  -a <appId> -i <installationId> -k <path/to/private-key.pem>
-```
-
-(An installation access token is then printed out to the standard output.)
-
-This source code is compiled using [@zeit/ncc](https://github.com/zeit/ncc) into a single `.js` file which is then published to `npm`, so it installs and runs fast!
+A simple CLI to create deployments and deployment statuses.
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,143 @@
 import { cli } from 'tkt'
-import { App } from '@octokit/app'
-import { readFileSync } from 'fs'
+import { readFileSync, writeFileSync } from 'fs'
+import { Octokit } from '@octokit/rest'
+
+const token = process.env['GITHUB_TOKEN']
+
+const octokit = new Octokit({
+  auth: token,
+  previews: ['ant-man-preview', 'flash-preview'],
+})
+
+if (!token) {
+  console.error('Missing environment variable GITHUB_TOKEN')
+  process.exit(1)
+}
 
 cli()
   .command(
-    '$0',
-    'Obtains a GitHub App Installation Token',
+    'create-deployment',
+    'Create a deployment',
     {
-      appId: {
-        alias: 'a',
-        description: 'App ID',
-        type: 'number',
+      owner: {
+        description: 'Owner parameter',
         demand: true,
-      },
-      installationId: {
-        alias: 'i',
-        description: 'App Installation ID',
-        type: 'number',
-        demand: true,
-      },
-      privateKey: {
-        alias: 'k',
-        description: 'Private Key',
         type: 'string',
+      },
+      repo: {
+        description: 'Repo parameter',
         demand: true,
+        type: 'string',
+      },
+      ref: {
+        description: 'The ref to deploy. This can be a branch, tag, or SHA',
+        demand: true,
+        type: 'string',
+      },
+      environment: {
+        description: 'Name for the target deployment environment',
+        type: 'string',
+      },
+      description: {
+        description: 'Short description of the deployment',
+        type: 'string',
+      },
+      transientEnvironment: {
+        description:
+          'Specifies if the given environment is specific to the deployment and will no longer exist at some point in the future',
+        type: 'boolean',
+      },
+      productionEnvironment: {
+        description:
+          'Specifies if the given environment is one that end-users directly interact with',
+        type: 'boolean',
       },
     },
     async args => {
-      const app = new App({
-        id: args.appId,
-        privateKey: readFileSync(args.privateKey, 'utf8'),
-      })
-      const installationAccessToken = await app.getInstallationAccessToken({
-        installationId: args.installationId,
-      })
-      console.log(installationAccessToken)
+      try {
+        const out = await octokit.repos.createDeployment({
+          owner: args.owner,
+          repo: args.repo,
+          ref: args.ref,
+          environment: args.environment,
+          transient_environment: args.transientEnvironment,
+          production_environment: args.productionEnvironment,
+        })
+        console.log(JSON.stringify(out.data, null, 2))
+      } catch (error) {
+        throw new Error('Cannot create deployment: ' + error)
+      }
+    },
+  )
+  .command(
+    'create-deployment-status',
+    'Create a deployment status',
+    {
+      owner: {
+        description: 'Owner parameter',
+        demand: true,
+        type: 'string',
+      },
+      repo: {
+        description: 'Repo parameter',
+        demand: true,
+        type: 'string',
+      },
+      deploymentId: {
+        description: 'Deployment ID parameter',
+        type: 'number',
+      },
+      deployment: {
+        description: 'Path to JSON deployment file',
+        type: 'string',
+      },
+      state: {
+        description:
+          'The state of the status. Can be one of error, failure, inactive, in_progress, queued, pending, or success',
+        demand: true,
+        type: 'string',
+      },
+      logUrl: {
+        description: "The full URL of the deployment's output",
+        type: 'string',
+      },
+      description: {
+        description:
+          'A short description of the status. The maximum description length is 140 characters',
+        type: 'string',
+      },
+      environmentUrl: {
+        description: 'Sets the URL for accessing your environment',
+        type: 'string',
+      },
+      autoInactive: {
+        description:
+          "Adds a new inactive status to all prior non-transient environment deployments with the same repository and environment name as the created status's deployment",
+        type: 'boolean',
+      },
+    },
+    async args => {
+      if (!args.deploymentId && !args.deployment) {
+        throw new Error('Must specify --deploymentId or --deployment')
+      }
+      try {
+        const deploymentId =
+          args.deploymentId ||
+          JSON.parse(readFileSync(args.deployment!, 'utf8'))
+        const out = await octokit.repos.createDeploymentStatus({
+          owner: args.owner,
+          repo: args.repo,
+          deployment_id: deploymentId,
+          state: args.state as any,
+          log_url: args.logUrl,
+          description: args.description,
+          environmentUrl: args.environmentUrl,
+          auto_inactive: args.autoInactive,
+        })
+        console.log(JSON.stringify(out.data, null, 2))
+      } catch (error) {
+        throw new Error('Cannot create deployment: ' + error)
+      }
     },
   )
   .parse()
